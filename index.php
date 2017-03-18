20f86111d81d642d5b91253812f0dfd9eeee9d82
telematica <telematica>
2017-03-18 11:17:13 -0500

    my app

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba Horoku App empresariales Darwin Meneses</title>
+  </head>
+  <body>
+    ?<?php
+      echo '<p>hola App empresariales Darwin</p>';
+    ?>
+  </body>
+</html>
